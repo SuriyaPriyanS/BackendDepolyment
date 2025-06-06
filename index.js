e08677316017e562ac539535b2421d18128845af
Suriya Priyan <suriyapriyan0506@gmail.com>
2025-06-06 13:29:03 +0530

    updatedcode

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import notesRouter from './Routers/notesRouter.js';
 import userRouter from './Routers/userRouter.js';
 import { PrismaClient } from '@prisma/client';
-import cors from 'cors'
+import cors from 'cors';
 
 // import { authMiddleware } from './middleware/auth.js';
 
@@ -13,20 +13,17 @@ const prisma = new PrismaClient(); // Instantiate PrismaClient
 const app = express();
 
 app.use(express.json());
-app.use(cors(
-    {
-        origin: '*', // Allow all origins, you can specify a specific origin if needed
-        methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific HTTP methods
-        allowedHeaders: ['Content-Type', 'Authorization'],
-        credentials: true // Allow credentials if needed
-    }
-)); // Enable CORS for all routes
+app.use(cors({
+    origin: '*', // For production, replace '*' with your frontend URLs
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
 
 // app.use(authMiddleware);
 
-
 app.use('/api/notes', notesRouter);
-app.use('/api/users', userRouter)
+app.use('/api/users', userRouter);
 
 const PORT = process.env.PORT || 5000;
 
@@ -35,6 +32,7 @@ if (!DATABASE_URL) {
   console.error('DATABASE_URL is not defined in .env file');
   process.exit(1);
 }
+
 prisma.$connect()
   .then(() => {
     console.log('Connected to the database');
@@ -43,8 +41,7 @@ prisma.$connect()
     console.error('Error connecting to the database:', error);
     process.exit(1);
   });
-    
-    
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
